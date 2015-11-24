f8f071d0b156876cd679f557d399141ad8f1cad4
Marina <arhipova.m.s@yandex.ru>
2015-11-24 12:03:38 +0300

    Initial commit

---
+++ src/ru.sbt.mp/Stack.java
@@ -0,0 +1,20 @@
+
+package  ru.sbt.mp;
+public class Stack {
+    public int a;
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi!");
+
+    }
+
+    public static void main() {
+        Stack stack = new Stack();
+        stack.a = 2;
+        System.out.println(stack.a);
+    }
+
+}
+
+
